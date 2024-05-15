3a245d13357e7035f5d0c5f8a1a2a0592a86fb39
Nixbu <83171543+Nixbu@users.noreply.github.com>
2024-05-15 11:52:32 +0300

    yes

--- include/Controller.h
+++ include/Controller.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
+#include "Menu.h"
 
 class Controller
 {
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,15 @@
+#include <exception>
+#include "Controller.h"
+
 int main()
 {
+	try {
+
+		Controller game;
+		game.run();
+	}
+	catch (const std::exception& e)
+	{
+
+	}
 }
